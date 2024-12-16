555e35126f0cbe2a83d7db181c2795d866434b7f
Arkhip Kupchenko <arhipq2007@gmail.com>
2024-12-17 00:46:18 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #6. The Wind Rose\n";
     cout << "Author: Arkhip Kupchenko\n";
+    cout << "Group: 24PING_1D\n";
     return 0;
 }
